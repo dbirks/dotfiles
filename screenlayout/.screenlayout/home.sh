f894ae61424c6362ea2595dc37689889eb321ff1
David Birks <david@tellus.space>
2017-12-02 22:01:49 -0500

    add home, other slight changes

---
+++ screenlayout/.screenlayout/home.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI3 --off --output HDMI2 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off --output DP1 --primary --mode 1280x960 --pos 1920x0 --rotate normal
