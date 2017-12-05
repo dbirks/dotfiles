fa471084e6fe377f43a8176342152b52833e10af
David Birks <davidebirks@gmail.com>
2017-12-05 09:34:29 -0500

    add basic polybar

--- screenlayout/.screenlayout/work.sh
+++ screenlayout/.screenlayout/work.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x624 --rotate normal --output DP-1-1 --off --output HDMI-1 --off --output DP-1-3 --mode 1920x1080 --pos 3000x0 --rotate left --output DP-1-2 --mode 1920x1080 --pos 1920x0 --rotate left --output DP-1 --off --output HDMI-2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x744 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP1-3 --off --output DP1-2 --mode 1920x1080 --pos 1920x0 --rotate left --output DP1-1 --mode 1920x1080 --pos 3000x0 --rotate right
+
+#apply bg
+sh ~/.fehbg
