a107e7e27604e934e0c70e37c34f882e08bbecf1
David Birks <davidebirks@gmail.com>
2018-07-25 15:17:28 -0400

    update some screen layouts

--- screenlayout/.screenlayout/work.sh
+++ screenlayout/.screenlayout/work.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 3000x736 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP1-3 --off --output DP1-2 --mode 1920x1080 --pos 1080x424 --rotate normal --output DP1-1 --mode 1920x1080 --pos 0x0 --rotate left
+xrandr --output VIRTUAL1 --off --output DP3 --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --primary --mode 1366x768 --pos 3840x472 --rotate normal --output VGA1 --off
 
 #apply bg
 feh --bg-fill --recursive --randomize "/home/david/Pictures/Wallpapers/super cute girl"
 
 # bar
 ~/.config/polybar/launch.sh
+
--- screenlayout/.screenlayout/work2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --mode 1920x1080 --pos 0x0 --rotate left --output DP2 --mode 1920x1080 --pos 1080x544 --rotate normal --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --primary --mode 1366x768 --pos 3000x856 --rotate normal --output VGA1 --off
-
-#apply bg
-feh --bg-fill --recursive --randomize "/home/david/Pictures/Wallpapers/super cute girl"
-
-# bar
-~/.config/polybar/launch.sh
