5f6e1940eaddef7dc08c239d58f3b4e4f23f535f
David Birks <davidebirks@gmail.com>
2018-07-25 15:16:12 -0400

    add other screen layouts

--- screenlayout/.screenlayout/work3.sh
+++ screenlayout/.screenlayout/work3.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
 xrandr --output VIRTUAL1 --off --output DP3 --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --primary --mode 1366x768 --pos 1920x640 --rotate normal --output VGA1 --off
+
+#apply bg
+feh --bg-fill --recursive --randomize "/home/david/Pictures/Wallpapers/super cute girl"
+
+# bar
+~/.config/polybar/launch.sh
+
